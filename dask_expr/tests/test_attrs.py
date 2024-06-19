3778d5fd2bfbbb3a566feb559dba68328d38458e
Luca Marconato <m.lucalmer@gmail.com>
2024-06-19 18:20:25 +0200

    fix pre-commit

--- dask_expr/tests/test_attrs.py
+++ dask_expr/tests/test_attrs.py
@@ -1,5 +1,7 @@
-import pandas as pd
 import dask.dataframe as dd
+import pandas as pd
+
+
 # luca
 # @pytest.mark.skipif(DASK_EXPR_ENABLED, reason="not important now")
 def test_attrs_dataframe():
@@ -35,6 +37,7 @@ def test_attrs_dataframe_not_shared_across_instances():
     assert df0.attrs != df1.attrs
     assert ddf1.attrs != ddf0.attrs
 
+
 def test_attrs_series_not_shared_across_instances():
     s0 = pd.Series([1, 2], name="A")
     s0.attrs["unit"] = "kg"
@@ -46,27 +49,29 @@ def test_attrs_series_not_shared_across_instances():
     assert s0.attrs != s1.attrs
     assert ds1.attrs != ds0.attrs
 
+
 def test_attrs_dataframe_optimize():
     df0 = pd.DataFrame({"A": [1, 2], "B": [3, 4], "C": [5, 6]})
     df0.attrs = {"date": "2020-10-16"}
     df = dd.from_pandas(df0)
-    df.attrs['foo'] = "foo"
+    df.attrs["foo"] = "foo"
     df = df.fillna(100)
     df = df["A"]
     print(df.attrs)
-    df.attrs['foo'] = "bar"
+    df.attrs["foo"] = "bar"
     print(df.attrs)
 
     assert df.optimize().attrs == df.attrs
+
 
 def test_attrs_series_optimize():
     s0 = pd.Series([1, 2], name="A")
     s0.attrs["unit"] = "kg"
     ds = dd.from_pandas(s0, 2)
-    ds.attrs['foo'] = "foo"
+    ds.attrs["foo"] = "foo"
     ds = ds.fillna(100)
     print(ds.attrs)
-    ds.attrs['foo'] = "bar"
+    ds.attrs["foo"] = "bar"
     print(ds.attrs)
 
     assert ds.optimize().attrs == ds.attrs
